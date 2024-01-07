24c136ca2d312182082eddfd7f6d4da1ff7e91b4
Serge <sc9500@proton.me>
2024-01-07 20:05:23 +0000

    Change time format to 24h

--- blocks.h
+++ blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "echo \"$(cat /sys/class/power_supply/BAT1/capacity)%\"",					5,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "date '+%b %d (%a) %H:%M'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
