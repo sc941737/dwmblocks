1af342f3a1d66a1295c4e05223cbe623fd8f8628
Serge <sc9500@proton.me>
2024-03-15 01:27:06 +0000

    Minor changes
    Change refresh times for date and battery
    Add xdotool windowkill util

--- blocks.h
+++ blocks.h
@@ -1,10 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+        {"", "printf ''", 							0, 		0},
 
-	{"", "echo \"$(cat /sys/class/power_supply/BAT1/capacity)%\"",					5,		0},
-	{"", "date '+%b %d (%a) %H:%M'",					5,		0},
+	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "echo \"$(cat /sys/class/power_supply/BAT1/capacity)%\"",		60,		0},
+
+	{"", "date '+%b %d (%a) %H:%M'",					60,		0},
+
+	{"X ", "xdotool getwindowfocus windowkill",				0,		9},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
