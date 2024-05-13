173dd3091827ace4b4cc26a998f82812088ab7a2
Serge <sc9500@proton.me>
2024-05-13 23:53:48 +0100

    Extract logic into scripts

--- blocks.h
+++ blocks.h
@@ -1,17 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-        {"", "printf ''", 							0, 		0},
+	{" ",		"blocks-volume",	0,			10},
 
-	{"🔊 ", "vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@); echo ${vol#*.}%",	0,		10},
+	{"",	 	"blocks-memory",	30,			0},
 
-	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"",	 	"blocks-power",		60,			0},
 
-	{"⚡ ", "echo \"$(cat /sys/class/power_supply/BAT1/capacity)%\"",		60,		0},
+	{"",	 	"blocks-time",		60,			0},
 
-	{"", "date '+%b %d (%a) %H:%M'",					60,		0},
-
-	{"X ", "xdotool windowclose $(xdotool getwindowfocus)",			0,		9},
+	{"❌ ",	 	"blocks-close-btn",	0,			9},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
