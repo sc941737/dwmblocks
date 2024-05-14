843d8b6066fc0cbbe5828fb84c35649bc52a802a
Serge <sc9500@proton.me>
2024-05-14 10:53:51 +0100

    Refresh volume once per minute
    This will sync it with volume changed by other apps

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ",		"blocks-volume",	0,			10},
+	{" ",		"blocks-volume",	60,			10},
 
 	{"",	 	"blocks-memory",	30,			0},
 
